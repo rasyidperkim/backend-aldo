40d016131f6351a25cd464f174d32d6f57133c6b
Abah Faqih <rasyidperkim@gmail.com>
2021-11-08 17:38:54 +0800

    update

--- app/Http/Requests/MassDestroyContentPageRequest.php
+++ app/Http/Requests/MassDestroyContentPageRequest.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Requests;
 
-use App\Models\ContentCategory;
+use App\Models\ContentPage;
 use Gate;
 use Illuminate\Foundation\Http\FormRequest;
 use Symfony\Component\HttpFoundation\Response;
 
-class MassDestroyContentCategoryRequest extends FormRequest
+class MassDestroyContentPageRequest extends FormRequest
 {
     public function authorize()
     {
-        abort_if(Gate::denies('content_category_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('content_page_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         return true;
     }
@@ -20,7 +20,7 @@ class MassDestroyContentCategoryRequest extends FormRequest
     {
         return [
             'ids'   => 'required|array',
-            'ids.*' => 'exists:content_categories,id',
+            'ids.*' => 'exists:content_pages,id',
         ];
     }
 }
--- app/Traits/Auditable.php
+++ app/Traits/Auditable.php
@@ -35,3 +35,4 @@ trait Auditable
             'host'         => request()->ip() ?? null,
         ]);
     }
+}
